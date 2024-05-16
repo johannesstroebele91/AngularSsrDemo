d898fe9fae35560c303d1fb9f8ad31a75dd1f5ff
johannesstroebele91 <stroebele.j@gmail.com>
2024-05-16 16:22:36 +0200

    Add a fake env secret (.env should normally not be exposed) to check if they work with Angular SRR

--- server.ts
+++ server.ts
@@ -4,6 +4,9 @@ import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
+import { config } from 'dotenv-esm';
+
+config(); // This will read the .env file and set environment variables
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -17,12 +20,22 @@ export function app(): express.Express {
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
 
+  server.get('/api/secret', (req, res) => {
+    // Fetch the secret from wherever it's stored
+    const secret = process.env['SECRET']; // Or fetch it from a database, file, etc.
+
+    // Return the secret as JSON
+    res.json({ secret });
+  });
+
+
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
   server.get('*.*', express.static(browserDistFolder, {
     maxAge: '1y'
   }));
+
 
   // All regular routes use the Angular engine
   server.get('*', (req, res, next) => {
@@ -44,6 +57,7 @@ export function app(): express.Express {
 }
 
 function run(): void {
+  console.log('process.env.SECRET', process.env['SECRET']); // Log process.env here
   const port = process.env['PORT'] || 4000;
 
   // Start up the Node server
@@ -52,5 +66,7 @@ function run(): void {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
+
+
 
 run();
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,11 +8,17 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   template: `
     <h1>Welcome to {{title}}!</h1>
-
+    <button (click)="getSecret()">Get</button>
     <router-outlet />
   `,
   styles: [],
 })
 export class AppComponent {
   title = 'AngularSsrDemo';
+
+  constructor(private http: HttpClient) { }
+
+  getSecret() {
+    console.log(this.http.get<{ secret: string }>('http://localhost:4000/api/secret').subscribe({next: (data) => console.log(data)}))
+  }
 }
